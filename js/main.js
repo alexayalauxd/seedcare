67d525cdd01a67c8a46282e8df04d1d8ea95ae30
alexayalauxd <alexayala.esp@gmail.com>
2021-01-11 21:31:37 -0600

    javascript states

--- js/main.js
+++ js/main.js
@@ -1,186 +1,85 @@
-!(function($) {
-  "use strict";
+"use strict";
 
-  // Preloader
-  $(window).on('load', function() {
-    if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function() {
-        $(this).remove();
-      });
-    }
-  });
 
-  // Smooth scroll for the navigation menu and links with .scrollto classes
-  var scrolltoOffset = $('#header').outerHeight() - 2;
-  $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      if (target.length) {
-        e.preventDefault();
+if (!mainSyngenta)
+	var mainSyngenta = {};
 
-        var scrollto = target.offset().top - scrolltoOffset;
+mainSyngenta.init = function () {
+	console.log('Init Syngenta Berries...');
+}
 
-        if ($(this).attr("href") == '#header') {
-          scrollto = 0;
-        }
+mainSyngenta.homeSwipper = function () {
+	let getSwipper = document.getElementById('HomeSwipper');
+	if (typeof (getSwipper) != 'undefined' && getSwipper != null) {
+		let mySwiper = new Swiper('#HomeSwipper', {
+			autoplay: {
+				delay: 3000,
+			},
+			effect: 'fade',
+			loop: true,
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+				renderBullet: function (index, className) {
+					return '<span class="' + className + '">' + (index + 1) + '</span>';
+				},
+			}
+		});
+	}
+}
 
-        $('html, body').animate({
-          scrollTop: scrollto
-        }, 1500, 'easeInOutExpo');
 
-        if ($(this).parents('.nav-menu, .mobile-nav').length) {
-          $('.nav-menu .active, .mobile-nav .active').removeClass('active');
-          $(this).closest('li').addClass('active');
-        }
+mainSyngenta.initForm = function () {
+	let getForm = document.getElementById('formulario');
+	const selectSate = document.getElementById('SelectMxState');
+	const selectLocal = document.getElementById('SelectMXLocal');
+	const formulario = document.getElementById('formulario');
+	const inputs = document.querySelectorAll('#formulario input');
 
-        if ($('body').hasClass('mobile-nav-active')) {
-          $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-          $('.mobile-nav-overly').fadeOut();
-        }
-        return false;
-      }
-    }
-  });
+	if (typeof (getForm) != 'undefined' && getForm != null) {
+		formulario.reset();
+		// init selectOptions
+		selectLocal.setAttribute('disabled', true);
+		// fillstates
+		function fillStates() {
+			selectSate.addEventListener('change', x => {
+				if (selectSate.options[0].value == "Selecciona un estado") {
+					selectLocal.removeAttribute('disabled');
+					selectSate.options[0] = null;
+				}
+				document.getElementById('grupo__state').classList.remove('formulario__grupo-incorrecto');
+				document.querySelector('#grupo__state .formulario__input-error').classList.remove('formulario__input-error-activo');
+				changeState(x.target.value);
+			})
+			for (const c_state in statesMX) {
+				const newOption = document.createElement('option');
+				const optionText = document.createTextNode(c_state);
+				newOption.appendChild(optionText);
+				newOption.setAttribute('value', c_state);
+				selectSate.appendChild(newOption);
+			}
+		}
 
-  // Activate smooth scroll on page load with hash links in the url
-  $(document).ready(function() {
-    if (window.location.hash) {
-      var initial_nav = window.location.hash;
-      if ($(initial_nav).length) {
-        var scrollto = $(initial_nav).offset().top - scrolltoOffset;
-        $('html, body').animate({
-          scrollTop: scrollto
-        }, 1500, 'easeInOutExpo');
-      }
-    }
-  });
+		function changeState(state) {
+			emptyElement(selectLocal);
+			for (let i = 0; i < statesMX[state].length; i++) {
+				const newOption = document.createElement('option');
+				const optionText = document.createTextNode(statesMX[state][i]);
+				newOption.appendChild(optionText);
+				newOption.setAttribute('value', statesMX[state][i]);
+				selectLocal.appendChild(newOption);
+			}
+		}
 
-  // Mobile Navigation
-  if ($('.nav-menu').length) {
-    var $mobile_nav = $('.nav-menu').clone().prop({
-      class: 'mobile-nav d-lg-none'
-    });
-    $('body').append($mobile_nav);
-    $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
-    $('body').append('<div class="mobile-nav-overly"></div>');
+		function emptyElement(elem) {
+			elem.innerHTML = '';
+		}
 
-    $(document).on('click', '.mobile-nav-toggle', function(e) {
-      $('body').toggleClass('mobile-nav-active');
-      $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-      $('.mobile-nav-overly').toggle();
-    });
 
-    $(document).on('click', '.mobile-nav .drop-down > a', function(e) {
-      e.preventDefault();
-      $(this).next().slideToggle(300);
-      $(this).parent().toggleClass('active');
-    });
 
-    $(document).click(function(e) {
-      var container = $(".mobile-nav, .mobile-nav-toggle");
-      if (!container.is(e.target) && container.has(e.target).length === 0) {
-        if ($('body').hasClass('mobile-nav-active')) {
-          $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-          $('.mobile-nav-overly').fadeOut();
-        }
-      }
-    });
-  } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
-    $(".mobile-nav, .mobile-nav-toggle").hide();
-  }
 
-  // Navigation active state on scroll
-  var nav_sections = $('section');
-  var main_nav = $('.nav-menu, #mobile-nav');
 
-  $(window).on('scroll', function() {
-    var cur_pos = $(this).scrollTop() + 200;
 
-    nav_sections.each(function() {
-      var top = $(this).offset().top,
-        bottom = top + $(this).outerHeight();
-
-      if (cur_pos >= top && cur_pos <= bottom) {
-        if (cur_pos <= bottom) {
-          main_nav.find('li').removeClass('active');
-        }
-        main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
-      }
-      if (cur_pos < 300) {
-        $(".nav-menu ul:first li:first").addClass('active');
-      }
-    });
-  });
-
-  // Toggle .header-scrolled class to #header when page is scrolled
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('#header').addClass('header-scrolled');
-    } else {
-      $('#header').removeClass('header-scrolled');
-    }
-  });
-
-  if ($(window).scrollTop() > 100) {
-    $('#header').addClass('header-scrolled');
-  }
-
-  // Back to top button
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn('slow');
-    } else {
-      $('.back-to-top').fadeOut('slow');
-    }
-  });
-
-  $('.back-to-top').click(function() {
-    $('html, body').animate({
-      scrollTop: 0
-    }, 1500, 'easeInOutExpo');
-    return false;
-  });
-
-  // jQuery counterUp
-  $('[data-toggle="counter-up"]').counterUp({
-    delay: 10,
-    time: 1000
-  });
-
-  // Porfolio isotope and filter
-  $(window).on('load', function() {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item'
-    });
-
-    $('#portfolio-flters li').on('click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
-      $(this).addClass('filter-active');
-
-      portfolioIsotope.isotope({
-        filter: $(this).data('filter')
-      });
-    });
-
-    // Initiate venobox (lightbox feature used in portofilo)
-    $(document).ready(function() {
-      $('.venobox').venobox({
-        'share': false
-      });
-    });
-  });
-
-  // Portfolio details carousel
-  $(".portfolio-details-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
-
-})(jQuery);
 
 		// Validaciones
 
@@ -252,7 +151,7 @@
 			}
 
 
-			if (!document.getElementById('RecieveInfoPapa').checked && !document.getElementById('RecieveInfoFrijol').checked && !document.getElementById('RecieveInfoArroz').checked && !document.getElementById('RecieveInfoCereal').checked && !document.getElementById('RecieveInfoMaíz').checked) {
+			if (!document.getElementById('RecieveInfoFresas').checked && !document.getElementById('RecieveInfoArandanos').checked && !document.getElementById('RecieveInfoMoras').checked && !document.getElementById('RecieveInfoFrambuesas').checked && !document.getElementById('RecieveInfoOtros').checked) {
 				document.querySelector('#grupo__cultivo .formulario__input-error').classList.add('formulario__input-error-activo');
 				validateAll.push(false);
 			} else {
@@ -347,20 +246,20 @@
 
 		function getFarming(){
 			let farming = "";
-			if (document.getElementById('RecieveInfoPapa').checked) {
-				farming += "Papa ";
+			if (document.getElementById('RecieveInfoFresas').checked) {
+				farming += "Fresas ";
 			}
-			if (document.getElementById('RecieveInfoFrijol').checked) {
-				farming += "Frijol ";
+			if (document.getElementById('RecieveInfoArandanos').checked) {
+				farming += "Arándanos ";
 			}
-			if (document.getElementById('RecieveInfoArroz').checked) {
-				farming += "Arroz ";
+			if (document.getElementById('RecieveInfoMoras').checked) {
+				farming += "Moras ";
 			}
-			if (document.getElementById('RecieveInfoCereal').checked) {
-				farming += "Cereal ";
+			if (document.getElementById('RecieveInfoFrambuesas').checked) {
+				farming += "Frambuesas ";
 			}
-			if (document.getElementById('RecieveInfoMaíz').checked) {
-				farming += "Maíz ";
+			if (document.getElementById('RecieveInfoOtros').checked) {
+				farming += "Otros ";
 			}
 			return farming;
 		}
@@ -525,4 +424,13 @@ function createExcel(excel_data, fromDate, toDate){
 		let blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
 		saveAs(blob,"Syngenta_Berries_Database.xlsx");
 	});
+}
+
+
+window.onload = function () {
+	mainSyngenta.init();
+	mainSyngenta.homeSwipper();
+	mainSyngenta.initForm();
+	mainSyngenta.initThankfull();
+	mainSyngenta.admininit();
 }
